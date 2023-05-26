18f04b806ed3c4b7de0a162ab9816a0ccfd1a900
ntaheij <noah@taheij.nl>
2023-05-26 18:04:27 +0200

    Fixed formatting & linting

--- src/script.js
+++ src/script.js
@@ -17,43 +17,41 @@
 
 */
 
-const paper = document.getElementById('paper'),
-  pen = paper.getContext('2d');
+const paper = document.getElementById('paper')
+const pen = paper.getContext('2d')
 
-const get = (selector) => document.querySelector(selector);
+const get = (selector) => document.querySelector(selector)
 
 const toggles = {
-  sound: get('#sound-toggle'),
-};
+  sound: get('#sound-toggle')
+}
 
 // Use whatever colors you want
 // https://unsplash.com/photos/tZCrFpSNiIQ
-// const colors = [
-//   "#D0E7F5",
-//   "#D9E7F4",
-//   "#D6E3F4",
-//   "#BCDFF5",
-//   "#B7D9F4",
-//   "#C3D4F0",
-//   "#9DC1F3",
-//   "#9AA9F4",
-//   "#8D83EF",
-//   "#AE69F0",
-//   "#D46FF1",
-//   "#DB5AE7",
-//   "#D911DA",
-//   "#D601CB",
-//   "#E713BF",
-//   "#F24CAE",
-//   "#FB79AB",
-//   "#FFB6C1",
-//   "#FED2CF",
-//   "#FDDFD5",
-//   "#FEDCD1"
-// ];
-// Here I'm using the same color for all 21 arcs
-const colors = Array(21).fill('#A6C48A');
-const imagesAmount = 11;
+const colors = [
+  '#D0E7F5',
+  '#D9E7F4',
+  '#D6E3F4',
+  '#BCDFF5',
+  '#B7D9F4',
+  '#C3D4F0',
+  '#9DC1F3',
+  '#9AA9F4',
+  '#8D83EF',
+  '#AE69F0',
+  '#D46FF1',
+  '#DB5AE7',
+  '#D911DA',
+  '#D601CB',
+  '#E713BF',
+  '#F24CAE',
+  '#FB79AB',
+  '#FFB6C1',
+  '#FED2CF',
+  '#FDDFD5',
+  '#FEDCD1'
+]
+const imagesAmount = 11
 
 const settings = {
   startTime: new Date().getTime(), // This can be in the future
@@ -61,73 +59,77 @@ const settings = {
   maxCycles: Math.max(colors.length, 100), // Must be above colors.length or else...
   soundEnabled: false, // User still must interact with screen first
   pulseEnabled: true, // Pulse will only show if sound is enabled as well
-  instrument: 'wave', // "default" | "wave" | "vibraphone"
-};
+  instrument: 'wave' // "default" | "wave" | "vibraphone"
+}
 
 const handleSoundToggle = (enabled = !settings.soundEnabled) => {
-  settings.soundEnabled = enabled;
-  toggles.sound.dataset.toggled = enabled;
-};
+  settings.soundEnabled = enabled
+  toggles.sound.dataset.toggled = enabled
+}
 
 const handleBackgroundToggle = () => {
-  const backgroundElem = document.getElementById('background-image');
-  var style =
-      backgroundElem.currentStyle ||
-      window.getComputedStyle(backgroundElem, false),
-    currentImage = style.backgroundImage.slice(4, -1).replace(/"/g, ''),
-    currentIndex = currentImage.split('/').pop().split('.')[0].split('-').pop();
+  const backgroundElem = document.getElementById('background-image')
+  const style =
+    backgroundElem.currentStyle ||
+    window.getComputedStyle(backgroundElem, false)
+  const currentImage = style.backgroundImage.slice(4, -1).replace(/"/g, '')
+  const currentIndex = currentImage
+    .split('/')
+    .pop()
+    .split('.')[0]
+    .split('-')
+    .pop()
 
   backgroundElem.style.backgroundImage = `url('img/backgrounds/${
     (parseInt(currentIndex) + 1) % imagesAmount
-  }.webp')`;
-};
+  }.webp')`
+}
 
 const handleShuffle = () => {
-  settings.startTime = new Date().getTime();
-  settings.duration = Math.floor(Math.random() * 400) + 700;
-  settings.maxCycles = Math.floor(Math.random() * 20) + 90;
-  settings.instrument = Array('default', 'wave', 'vibraphone')[
+  settings.startTime = new Date().getTime()
+  settings.duration = Math.floor(Math.random() * 400) + 700
+  settings.maxCycles = Math.floor(Math.random() * 20) + 90
+  settings.instrument = ['default', 'wave', 'vibraphone'][
     Math.floor(Math.random() * 3)
-  ];
-  init();
-  draw();
-  handleBackgroundToggle();
-};
+  ]
+  init()
+  draw()
+  handleBackgroundToggle()
+}
 
-document.onvisibilitychange = () => hideText(false);
-document.onvisibilitychange = () => handleSoundToggle(false);
+document.onvisibilitychange = () => handleSoundToggle(false)
 
-paper.onclick = () => handleSoundToggle();
+paper.onclick = () => handleSoundToggle()
 
 const getFileName = (index) => {
-  if (settings.instrument === 'default') return `key-${index}`;
+  if (settings.instrument === 'default') return `key-${index}`
 
-  return `${settings.instrument}-key-${index}`;
-};
+  return `${settings.instrument}-key-${index}`
+}
 
 const getUrl = (index) =>
-  `https://assets.codepen.io/1468070/${getFileName(index)}.wav`;
+  `https://assets.codepen.io/1468070/${getFileName(index)}.wav`
 
 const keys = colors.map((color, index) => {
-  const audio = new Audio(getUrl(index));
+  const audio = new window.Audio(getUrl(index))
 
-  audio.volume = 0.15;
+  audio.volume = 0.15
 
-  return audio;
-});
+  return audio
+})
 
-let arcs = [];
+let arcs = []
 
 const calculateVelocity = (index) => {
-  const numberOfCycles = settings.maxCycles - index,
-    distancePerCycle = 2 * Math.PI;
+  const numberOfCycles = settings.maxCycles - index
+  const distancePerCycle = 2 * Math.PI
 
-  return (numberOfCycles * distancePerCycle) / settings.duration;
-};
+  return (numberOfCycles * distancePerCycle) / settings.duration
+}
 
 const calculateNextImpactTime = (currentImpactTime, velocity) => {
-  return currentImpactTime + (Math.PI / velocity) * 1000;
-};
+  return currentImpactTime + (Math.PI / velocity) * 1000
+}
 
 const calculateDynamicOpacity = (
   currentTime,
@@ -136,12 +138,12 @@ const calculateDynamicOpacity = (
   maxOpacity,
   duration
 ) => {
-  const timeSinceImpact = currentTime - lastImpactTime,
-    percentage = Math.min(timeSinceImpact / duration, 1),
-    opacityDelta = maxOpacity - baseOpacity;
+  const timeSinceImpact = currentTime - lastImpactTime
+  const percentage = Math.min(timeSinceImpact / duration, 1)
+  const opacityDelta = maxOpacity - baseOpacity
 
-  return maxOpacity - opacityDelta * percentage;
-};
+  return maxOpacity - opacityDelta * percentage
+}
 
 const determineOpacity = (
   currentTime,
@@ -150,7 +152,7 @@ const determineOpacity = (
   maxOpacity,
   duration
 ) => {
-  if (!settings.pulseEnabled) return baseOpacity;
+  if (!settings.pulseEnabled) return baseOpacity
 
   return calculateDynamicOpacity(
     currentTime,
@@ -158,89 +160,92 @@ const determineOpacity = (
     baseOpacity,
     maxOpacity,
     duration
-  );
-};
+  )
+}
 
 const calculatePositionOnArc = (center, radius, angle) => ({
   x: center.x + radius * Math.cos(angle),
-  y: center.y + radius * Math.sin(angle),
-});
+  y: center.y + radius * Math.sin(angle)
+})
 
-const playKey = (index) => keys[index].play();
+const playKey = (index) => keys[index].play()
 
 const init = () => {
-  pen.lineCap = 'round';
+  pen.lineCap = 'round'
 
   arcs = colors.map((color, index) => {
-    const velocity = calculateVelocity(index),
-      lastImpactTime = 0,
-      nextImpactTime = calculateNextImpactTime(settings.startTime, velocity);
+    const velocity = calculateVelocity(index)
+    const lastImpactTime = 0
+    const nextImpactTime = calculateNextImpactTime(
+      settings.startTime,
+      velocity
+    )
 
     return {
       color,
       velocity,
       lastImpactTime,
-      nextImpactTime,
-    };
-  });
-};
+      nextImpactTime
+    }
+  })
+}
 
 const drawArc = (x, y, radius, start, end, action = 'stroke') => {
-  pen.beginPath();
+  pen.beginPath()
 
-  pen.arc(x, y, radius, start, end);
+  pen.arc(x, y, radius, start, end)
 
-  if (action === 'stroke') pen.stroke();
-  else pen.fill();
-};
+  if (action === 'stroke') pen.stroke()
+  else pen.fill()
+}
 
 const drawPointOnArc = (center, arcRadius, pointRadius, angle) => {
-  const position = calculatePositionOnArc(center, arcRadius, angle);
+  const position = calculatePositionOnArc(center, arcRadius, angle)
 
-  drawArc(position.x, position.y, pointRadius, 0, 2 * Math.PI, 'fill');
-};
+  drawArc(position.x, position.y, pointRadius, 0, 2 * Math.PI, 'fill')
+}
 
 const draw = () => {
   // Definitely not optimized
-  paper.width = paper.clientWidth;
-  paper.height = paper.clientHeight;
+  paper.width = paper.clientWidth
+  paper.height = paper.clientHeight
 
-  const currentTime = new Date().getTime(),
-    elapsedTime = (currentTime - settings.startTime) / 1000;
+  const currentTime = new Date().getTime()
+  const elapsedTime = (currentTime - settings.startTime) / 1000
 
-  const length = Math.min(paper.width, paper.height) * 0.9,
-    offset = (paper.width - length) / 2;
+  const length = Math.min(paper.width, paper.height) * 0.9
+  const offset = (paper.width - length) / 2
 
   const start = {
     x: offset,
-    y: paper.height / 2,
-  };
+    y: paper.height / 2
+  }
 
   const end = {
     x: paper.width - offset,
-    y: paper.height / 2,
-  };
+    y: paper.height / 2
+  }
 
   const center = {
     x: paper.width / 2,
-    y: paper.height / 2,
-  };
+    y: paper.height / 2
+  }
 
   const base = {
     length: end.x - start.x,
     minAngle: 0,
     startAngle: 0,
-    maxAngle: 2 * Math.PI,
-  };
+    maxAngle: 2 * Math.PI
+  }
 
-  base.initialRadius = base.length * 0.05;
-  base.circleRadius = base.length * 0.006;
-  base.clearance = base.length * 0.03;
+  base.initialRadius = base.length * 0.05
+  base.circleRadius = base.length * 0.006
+  base.clearance = base.length * 0.03
   base.spacing =
-    (base.length - base.initialRadius - base.clearance) / 2 / colors.length;
+    (base.length - base.initialRadius - base.clearance) / 2 / colors.length
 
   arcs.forEach((arc, index) => {
-    const radius = base.initialRadius + base.spacing * index;
+    const radius = base.initialRadius + base.spacing * index
 
     // Draw arcs
     pen.globalAlpha = determineOpacity(
@@ -249,15 +254,15 @@ const draw = () => {
       0.15,
       0.65,
       1000
-    );
-    pen.lineWidth = base.length * 0.002;
-    pen.strokeStyle = arc.color;
+    )
+    pen.lineWidth = base.length * 0.002
+    pen.strokeStyle = arc.color
 
-    const offset = (base.circleRadius * (5 / 3)) / radius;
+    const offset = (base.circleRadius * (5 / 3)) / radius
 
-    drawArc(center.x, center.y, radius, Math.PI + offset, 2 * Math.PI - offset);
+    drawArc(center.x, center.y, radius, Math.PI + offset, 2 * Math.PI - offset)
 
-    drawArc(center.x, center.y, radius, offset, Math.PI - offset);
+    drawArc(center.x, center.y, radius, offset, Math.PI - offset)
 
     // Draw impact points
     pen.globalAlpha = determineOpacity(
@@ -266,36 +271,35 @@ const draw = () => {
       0.15,
       0.85,
       1000
-    );
-    pen.fillStyle = arc.color;
+    )
+    pen.fillStyle = arc.color
 
-    drawPointOnArc(center, radius, base.circleRadius * 0.75, Math.PI);
+    drawPointOnArc(center, radius, base.circleRadius * 0.75, Math.PI)
 
-    drawPointOnArc(center, radius, base.circleRadius * 0.75, 2 * Math.PI);
+    drawPointOnArc(center, radius, base.circleRadius * 0.75, 2 * Math.PI)
 
     // Draw moving circles
-    pen.globalAlpha = 1;
-    pen.fillStyle = arc.color;
+    pen.globalAlpha = 1
+    pen.fillStyle = arc.color
 
     if (currentTime >= arc.nextImpactTime) {
       if (settings.soundEnabled) {
-        playKey(index);
-        arc.lastImpactTime = arc.nextImpactTime;
+        playKey(index)
+        arc.lastImpactTime = arc.nextImpactTime
       }
 
       arc.nextImpactTime = calculateNextImpactTime(
         arc.nextImpactTime,
         arc.velocity
-      );
+      )
     }
 
-    const distance = elapsedTime >= 0 ? elapsedTime * arc.velocity : 0,
-      angle = (Math.PI + distance) % base.maxAngle;
+    const distance = elapsedTime >= 0 ? elapsedTime * arc.velocity : 0
+    const angle = (Math.PI + distance) % base.maxAngle
 
-    drawPointOnArc(center, radius, base.circleRadius, angle);
-  });
+    drawPointOnArc(center, radius, base.circleRadius, angle)
+  })
+  window.requestAnimationFrame(draw)
+}
 
-  requestAnimationFrame(draw);
-};
-
-handleShuffle();
+handleShuffle()
